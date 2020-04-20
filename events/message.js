a7f041de1d9ab7b05cf55f71582aeb65c29a2325
Shoty <45360547+mufeShot@users.noreply.github.com>
2020-04-20 22:30:43 +0200

    Add files via upload

--- events/message.js
+++ events/message.js
@@ -1,5 +1,11 @@
 const kick = require("../commands/kick");
 
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("Pong!")
+  }
+})
+
 module.exports = (client, message) => {
   if (message.content.startsWith("!kick")) {
     return kick(message);
